ed9f0bc521affc01167489a6fa7192c0ae07ec7c
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 21:06:19 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #write your code below this line
+    intList = []
+    max = 0
+    while(True):
+      number = int(input())
+      if(number==-1):
+        break
+      intList.append(number)
+
+    searchNumber = int(input("Search for?"))
+    for i in range(len(intList)):
+      if(intList[i] == searchNumber):
+        print("%s is at index %s"%(intList[i],i))
+
 
 if __name__ == '__main__':
     main()
